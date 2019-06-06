693aa28f1f09f3bce8048f15502e71cc2068cbaf
Ahad Badruddin <ahad.badruddin@gmail.com>
2019-06-06 14:38:01 -0400

    update

--- tt2/run/src/models.py
+++ tt2/run/src/models.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from mappers import Database 
+import json
+import requests
+
+
+
 
 class User: 
 
@@ -8,17 +13,40 @@ class User:
         self.username= username
         self.password= password
 
-    def fetch_username(self):
+    def login(self):
         with Database() as d:
             d.cursor.execute(
-                '''
-                SELECT username FROM users;''')
-        all_usernames = d.cursor.fetchall()
-       
-    def fetch_password(self):
-        pass
+               '''
+                SELECT password  FROM users
+                WHERE username = '{self.username}'
+                ;''')
+        password = d.cursor.fetchone()[0]
+        if password:
+            if self.password== password:
+                return True
+            return False
+
+
+def lookup(company_name):
+        api= lookup_api()
+        query= api + company_name
+        return json.loads(requests.get(query).text)[0]['Symbol']
+
+def quote(symbol):
+        api= quote_api()
+        query= api + symbol
+        return json.loads(requests.get(query).text)['LastPrice']
+    
+def quote_api():
+        endpoint= 'http://dev.markitondemand.com/MODApis/api/v2/Quote/json?symbol='
+        return endpoint
+
+def lookup_api():
+        endpoint= 'http://dev.markitondemand.com/MODApis/api/v2/Lookup/json?input='
+        return endpoint
 
 
 if __name__=='__main__':
     user = User('ahad','swordfish')
-    print(user.fetch_username())
+ #   print(user.fetch_username())
+#    print(lookup('ford'))
---
+++ tt2/run/src/schema.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sqlite3
+
+class Schema:
+    def __init__(self):
+        self.connection = sqlite3.connect('trader.db')
+        self.cursor = self.connection.cursor()
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self,type_,value,traceback):
+        if self.connection:
+            if self.cursor:
+                self.connection.commit()
+                self.cursor.close()
+            self.connection.close()
+
+    def create_table(self, table_name):
+        self.cursor.execute(
+            f' DROP TABLE IF EXISTS {table_name};')
+        self.cursor.execute(
+            f''' CREATE TABLE {table_name}(
+                pk INTEGER PRIMARY KEY AUTOINCREMENT)
+           ;''')    
+    
+    def add_column(self,table_name,column_name,column_type):
+        self.cursor.execute(
+            f'''ALTER TABLE {table_name}
+                ADD COLUMN {column_name} {column_type}
+           ;''')
+
+if __name__ == '__main__':
+    
+   # Schema().create_table('userinfo')
+    Schema().add_column('userinfo','password','VARCHAR')
+    Schema().add_column('userinfo','balance','FLOAT')
